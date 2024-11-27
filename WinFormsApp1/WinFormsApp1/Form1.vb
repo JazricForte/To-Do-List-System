e2dea2708eafb4bf2af4fd1f49aef9284392c9c5
Jazric Forte <jazricfortexx@gmail.com>
2024-11-27 10:57:54 +0800

    Rob added login and sign up features

---
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,49 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Dim conn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+    Dim READER As MySqlDataReader
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=127.0.0.1;userid=root;password='';database=todolistdb"
+        Dim READER As MySqlDataReader
+
+        Try
+            conn.Open()
+            Dim query As String
+            query = "SELECT * FROM todolistdb.usersign WHERE username = '" & TextBox1.Text & "' AND password = '" & TextBox2.Text & "' "
+            COMMAND = New MySqlCommand(query, conn)
+            READER = COMMAND.ExecuteReader
+            Dim count As Integer
+            count = 0
+
+            While READER.Read()
+                count += 1
+
+            End While
+
+            If count = 1 Then
+                MessageBox.Show("Username and Password are correct")
+                Form3.Show()
+                Me.Hide()
+            ElseIf count > 1 Then
+                MessageBox.Show("Username and Password are Duplicate")
+            Else
+                MessageBox.Show("Username and Password are not correct")
+            End If
+
+            conn.Close()
+
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form2.Show()
+        Me.Hide()
+    End Sub
+End Class
---
+++ WinFormsApp1/WinFormsApp1/Form2.vb
@@ -0,0 +1,45 @@
+﻿Imports MySql.Data.MySqlClient
+Imports Mysqlx.XDevAPI.Common
+
+Public Class Form2
+    Dim Gender As String
+    Dim conn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+    Dim DBDataSet As New DataTable
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=127.0.0.1;userid=root;password='';database=todolistdb"
+
+
+        Try
+            conn.Open()
+
+            Dim checkQuery As String = "SELECT COUNT(*) FROM todolistdb.usersign WHERE username = '" & TextBox1.Text.Replace("'", "''") & "'"
+            Dim checkCommand As New MySqlCommand(checkQuery, conn)
+            Dim userExists As Integer = Convert.ToInt32(checkCommand.ExecuteScalar())
+
+            If userExists > 0 Then
+                MessageBox.Show("Username is already taken. Please choose another one.")
+                Exit Sub
+            End If
+
+            Dim query As String
+            query = "INSERT INTO todolistdb.usersign (username, password) Values ('" & TextBox1.Text & "', '" & TextBox2.Text & "')"
+            COMMAND = New MySqlCommand(query, conn)
+
+
+            Dim result As Integer = COMMAND.ExecuteNonQuery()
+            If result > 0 Then
+                MessageBox.Show("Registered Successfully")
+                Form1.Show()
+                Me.Close()
+            Else
+                MessageBox.Show("Registration Failed")
+            End If
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+End Class
